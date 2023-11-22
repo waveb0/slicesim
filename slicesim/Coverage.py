fe1ba6b260815090793284f7588d3205e4b42a5f
waveb0 <96556800+waveb0@users.noreply.github.com>
2023-11-22 17:16:11 +0800

    Coverage.py

--- slicesim/Coverage.py
+++ slicesim/Coverage.py
@@ -1,15 +1,15 @@
 import math
 
 
-class Coverage:
+class Coverage: #覆盖范围
     def __init__(self, center, radius):
-        self.center = center
-        self.radius = radius
+        self.center = center #基站中心（位置x，y）
+        self.radius = radius #半径
 
     def _get_gaussian_distance(self, p):
         return math.sqrt(sum((i-j)**2 for i,j in zip(p, self.center)))
 
-    def is_in_coverage(self, x, y):
+    def is_in_coverage(self, x, y): #判断是否在基站的覆盖范围内
         return self._get_gaussian_distance((x,y)) <= self.radius
 
     def __str__(self):
