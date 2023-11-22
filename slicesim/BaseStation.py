c45989555b7737b33bc80c31bacc0df9dd53da96
waveb0 <96556800+waveb0@users.noreply.github.com>
2023-11-22 17:06:26 +0800

    BaseStation.py

--- slicesim/BaseStation.py
+++ slicesim/BaseStation.py
@@ -1,9 +1,9 @@
 class BaseStation:
     def __init__(self, pk, coverage, capacity_bandwidth, slices=None):
         self.pk = pk
-        self.coverage = coverage
-        self.capacity_bandwidth = capacity_bandwidth
-        self.slices = slices
+        self.coverage = coverage #基站覆盖范围
+        self.capacity_bandwidth = capacity_bandwidth #基站带宽
+        self.slices = slices #基站部署切片
         print(self)
 
     def __str__(self):
